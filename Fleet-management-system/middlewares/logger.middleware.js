391c583c673bdce9e83818a3b20a1c8b79795342
bhavya <bhavyareddy506@gmail.com>
2026-01-31 19:43:17 +0530

    message

---
+++ Fleet-management-system/middlewares/logger.middleware.js
@@ -0,0 +1,6 @@
+import fs from "fs";
+export const logger = (req, res, next) => {
+    const log = `${new Date().toISOString()} | ${req.method} | ${req.url}\n`;
+    fs.appendFileSync("logs.txt", log);
+    next();
+}
